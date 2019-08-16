109f745b719e1c0a925f2ed4b70af8eced8d2a19
Calvin Sauer <calvin.j.sauer@gmail.com>
2019-08-16 15:48:57 -0400

    Revert flag injection; we can just test directly for ReactNativeWebView existence

--- App.js
+++ App.js
@@ -13,14 +13,6 @@ const Colors = {
 export default function App() {
   usePushNotificationHandler(this);
 
-  /*
-   * Inject "flag" object for mobile app detection
-   */
-  const runFirst = `
-    document.OCCarrotMobile = {};
-    true;
-  `;
-
   return (
     <View style={styles.container}>
       <StatusBar backgroundColor={Colors.background} barStyle="dark-content" />
@@ -29,7 +21,6 @@ export default function App() {
         source={{ uri: webViewUrl }}
         style={{ marginTop: 30 }}
         onMessage={(event) => handleWebMessage(this.webref, event)}
-        injectedJavaScript={runFirst}
       />
     </View>
   );
