ac88a1482cccd3eaf2af8e4552a2a473152fc9fe
Calvin Sauer <calvin.j.sauer@gmail.com>
2019-08-16 14:58:02 -0400

    Inject flag so web can detect when its running in Expo shell

--- App.js
+++ App.js
@@ -13,6 +13,14 @@ const Colors = {
 export default function App() {
   usePushNotificationHandler(this);
 
+  /*
+   * Inject "flag" object for mobile app detection
+   */
+  const runFirst = `
+    document.OCCarrotMobile = {};
+    true;
+  `;
+
   return (
     <View style={styles.container}>
       <StatusBar backgroundColor={Colors.background} barStyle="dark-content" />
@@ -21,6 +29,7 @@ export default function App() {
         source={{ uri: webViewUrl }}
         style={{ marginTop: 30 }}
         onMessage={(event) => handleWebMessage(this.webref, event)}
+        injectedJavaScript={runFirst}
       />
     </View>
   );
