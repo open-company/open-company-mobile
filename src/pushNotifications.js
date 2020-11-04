f2827788efb9ce5fcbb47688e48fdd7d67f021b8
bago2k4 <iacopo@rc0.eu>
2020-11-04 17:52:39 +0100

    Update code to request for push notifications permissions to make sure it still works with the latest expo. Configure the notifications default channel on Android.

--- src/pushNotifications.js
+++ src/pushNotifications.js
@@ -1,5 +1,6 @@
 import * as Notifications from 'expo-notifications';
 import * as Permissions from 'expo-permissions';
+import Constants from 'expo-constants';
 
 /*
 Displays the push notification permission dialog to the user, and returns the push token
@@ -8,24 +9,41 @@ denied. Note that this function is truly only relevant on iOS devices. On Androi
 are granted upon installation. If you just wish to retrieve the device's unique push token without
 displaying the permission dialog, use the getPushNotificationToken() function.
 */
+
 export async function requestPushNotificationPermission() {
-    const { status: existingStatus } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
-    let finalStatus = existingStatus;
+    let token;
+    if (Constants.isDevice) {
 
-    // only ask if permissions have not already been determined, because
-    // iOS won't necessarily prompt the user a second time.
-    if (existingStatus !== 'granted') {
-        // Android remote notification permissions are granted during the app
-        // install, so this will only ask on iOS
-        const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
-        finalStatus = status;
+        const { status: existingStatus } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+        let finalStatus = existingStatus;
+
+        // only ask if permissions have not already been determined, because
+        // iOS won't necessarily prompt the user a second time.
+        if (existingStatus !== 'granted') {
+            // Android remote notification permissions are granted during the app
+            // install, so this will only ask on iOS
+            const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+            finalStatus = status;
+        }
+
+        // Stop here if the user did not grant permissions
+        if (finalStatus !== 'granted') {
+            return null;
+        }
+
+        // Get the token that uniquely identifies this device
+        token = (await Notifications.getExpoPushTokenAsync()).data;
     }
 
-    // Stop here if the user did not grant permissions
-    if (finalStatus !== 'granted') {
-        return null;
+    // On Android we need to setup at least the notifications default channel
+    if (Platform.OS === 'android') {
+        Notifications.setNotificationChannelAsync('default', {
+            name: 'default',
+            importance: Notifications.AndroidImportance.MAX,
+            vibrationPattern: [0, 250, 250, 250],
+            lightColor: '#FFFBFAF9',
+        });
     }
 
-    // Get the token that uniquely identifies this device
-    return Notifications.getExpoPushTokenAsync();
+    return token;
 }
